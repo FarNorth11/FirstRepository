72cfc35c0536aee0f4017fe9e7ea36f4d657e373
Kalen Pei <peichenkai11@gmail.com>
2017-05-16 14:18:28 +0800

    提交第二次

---
+++ upload2/upload2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  upload2
+//
+//  Created by Ben on 2017/5/16.
+//  Copyright © 2017年 Het. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
