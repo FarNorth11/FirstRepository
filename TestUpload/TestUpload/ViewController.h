791d11e3ffe7ad738d0d1554ddecaac60a1d7925
Kalen Pei <peichenkai11@gmail.com>
2017-05-16 11:29:58 +0800

    传一传

---
+++ TestUpload/TestUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUpload
+//
+//  Created by Ben on 2017/5/16.
+//  Copyright © 2017年 Het. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestUpload/TestUpload/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestUpload
+//
+//  Created by Ben on 2017/5/16.
+//  Copyright © 2017年 Het. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
